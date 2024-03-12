5532570588ea52a525350f7b82983221e3fd44c9
Mercen <mercen@mercen.net>
2024-03-12 21:23:01 +0900

    feat: Add Info.plist Values

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -1,35 +1,13 @@
 import ProjectDescription
 import ProjectDescriptionHelpers
 
-let schemes: [Scheme] = [
-    .scheme(
-        name: "DodamDodam-DEBUG",
-        shared: true,
-        buildAction: .buildAction(targets: ["DodamDodam"]),
-        runAction: .runAction(configuration: .debug),
-        archiveAction: .archiveAction(configuration: .debug),
-        profileAction: .profileAction(configuration: .debug),
-        analyzeAction: .analyzeAction(configuration: .debug)
-    ),
-    .scheme(
-        name: "DodamDodam-RELEASE",
-        shared: true,
-        buildAction: .buildAction(targets: ["DodamDodam"]),
-        testAction: nil,
-        runAction: .runAction(configuration: .release),
-        archiveAction: .archiveAction(configuration: .release),
-        profileAction: .profileAction(configuration: .release),
-        analyzeAction: .analyzeAction(configuration: .release)
-    )
-]
-
 let project = Project(
     name: "DodamDodam",
     settings: .settings(
         base: .init(),
         configurations: [
             .debug(name: .debug),
-            .release(name: .release)
+            .release(name: .release),
         ],
         defaultSettings: .recommended
     ),
@@ -39,8 +17,14 @@ let project = Project(
             destinations: .iOS,
             product: .app,
             bundleId: "com.b1nd.dodam",
+            deploymentTargets: .iOS("15.0"),
             infoPlist: .extendingDefault(
                 with: [
+                    "CFBundleDisplayName": "도담도담",
+                    "CFBundleShortVersionString": "1.0",
+                    "CFBundleVersion": "1",
+                    "UIMainStoryboardFile": "",
+                    "UILaunchStoryboardName": "LaunchScreen",
                     "NSAllowsArbitraryLoads": true,
                 ]
             ),
@@ -51,6 +35,5 @@ let project = Project(
                 .external(name: "Alamofire"),
             ]
         )
-    ],
-    schemes: schemes
+    ]
 )
---
+++ Projects/App/Source/Feature/Main/MainView.swift
@@ -0,0 +1,14 @@
+//
+//  MainView.swift
+//  DodamDodam
+//
+//  Created by Mercen on 3/12/24.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world")
+    }
+}
