3c273eb5932adf022541f092aef136fb891a5874
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2024-03-22 12:25:22 +0900

    feat: Add out segnmented view

---
+++ Projects/App/Source/Feature/Out/Component/OutSegmentedView.swift
@@ -0,0 +1,73 @@
+//
+//  OutSegmentedView.swift
+//  DodamDodam
+//
+//  Created by 이민규 on 3/22/24.
+//
+
+import SwiftUI
+import DDS
+
+struct OutSegmentedView: View {
+    
+    public init(
+        selection: Binding<Int>? = nil
+    ) {
+        self.selection = selection
+        let selected = selection?.wrappedValue ?? 0
+        self.selected = selected
+        self.animatedSelection = selected
+    }
+    
+    @Namespace private var animation
+    @State private var selection: Binding<Int>?
+    @State private var selected: Int
+    @State private var animatedSelection: Int
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            ForEach(0..<2) { idx in
+                let isSelected = animatedSelection == idx
+                Button {
+                    if selected != idx {
+                        selected = idx
+                        withAnimation(.spring(duration: 0.2)) {
+                            animatedSelection = idx
+                        }
+                    }
+                } label: {
+                    Text("외출 외박")
+                        .font(.title(.small))
+                        .dodamColor(.onSurfaceVariant)
+                        .frame(height: 44)
+                        .frame(maxWidth: .infinity)
+                }
+                .background(
+                    Group {
+                        if isSelected {
+                            RoundedRectangle(cornerRadius: 10)
+                                .dodamFill(.surfaceContainer)
+                                .matchedGeometryEffect(
+                                    id: 0,
+                                    in: animation
+                                )
+                                .shadow(
+                                    color: .black.opacity(0.1),
+                                    radius: 4, x: 2, y: 2
+                                )
+                        }
+                    }
+                )
+                .padding([.horizontal, .vertical], 4)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .background(Dodam.color(.secondary))
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+    }
+}
+
+#Preview {
+    OutSegmentedView()
+        .padding(.horizontal, 16)
+}
