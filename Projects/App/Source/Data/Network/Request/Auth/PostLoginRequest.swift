a4d7a6c750968de300d28ca1c5cb01ace46045a1
Mercen <mercen@mercen.net>
2024-04-01 21:18:27 +0900

    fix: Fix Request Dependency

--- Projects/App/Source/Data/Network/Request/Auth/PostLoginRequest.swift
+++ Projects/App/Source/Data/Network/Request/Auth/PostLoginRequest.swift
@@ -5,8 +5,6 @@
 //  Created by Mercen on 3/14/24.
 //
 
-import Foundation
-
 struct PostLoginRequest: RequestProtocol {
     
     let id: String
--- Projects/App/Source/Data/Network/Request/Auth/PostReissueRequest.swift
+++ Projects/App/Source/Data/Network/Request/Auth/PostReissueRequest.swift
@@ -5,8 +5,6 @@
 //  Created by Mercen on 3/14/24.
 //
 
-import Foundation
-
 struct PostReissueRequest: RequestProtocol {
     
     let refreshToken: String
