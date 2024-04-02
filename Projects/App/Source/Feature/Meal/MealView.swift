90bffb07a973542ffe634db7b9ce918b0d0ff4aa
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-04-02 22:20:57 +0900

    feat: Add meal logic

--- Projects/App/Source/Feature/Meal/MealView.swift
+++ Projects/App/Source/Feature/Meal/MealView.swift
@@ -66,13 +66,27 @@ struct MealView: View {
                             ForEach(0..<3, id: \.self) { idx in
                                 let data: Meal? = {
                                     switch idx {
-                                    case 0: return datas.breakfast
-                                    case 1: return datas.lunch
-                                    case 2: return datas.dinner
-                                    default: return nil
+                                    case 0: datas.breakfast
+                                    case 1: datas.lunch
+                                    case 2: datas.dinner
+                                    default: nil
                                     }
                                 }()
-                                if let data = data {
+                                let date: Date = {
+                                    let dateFormatter = DateFormatter()
+                                    dateFormatter.dateFormat = "yyyy-MM-dd"
+                                    guard let date = dateFormatter.date(from: datas.date) else {
+                                        return .now
+                                    }
+                                    return date
+                                }()
+                                
+                                let isMealTime = isMealTime(date, mealType: idx)
+                                
+                                let backgroundColor = isMealTime ? Dodam.color(.primary) : Dodam.color(.onSurfaceVariant)
+                                
+                                if let data = data
+                                {
                                     VStack(spacing: 16) {
                                         HStack(spacing: 12) {
                                             ZStack {
@@ -88,7 +102,7 @@ struct MealView: View {
                                                 .dodamColor(.onPrimary)
                                             }
                                             .frame(width: 52, height: 27)
-                                            .background(Dodam.color(.primary))
+                                            .background(backgroundColor)
                                             .clipShape(RoundedRectangle(cornerRadius: 32))
                                             Spacer()
                                             Text("\(Int(data.calorie))Kcal")
@@ -122,6 +136,44 @@ struct MealView: View {
         .background(Dodam.color(.surface))
         .task {
             await viewModel.onAppear()
+        }
+    }
+    
+    func isMealTime(_ date: Date, mealType: Int) -> Bool {
+        let calendar = Calendar.current
+        let currentDate = Date()
+        
+        // 현재 시간의 년, 월, 일
+        let currentYear = calendar.component(.year, from: currentDate)
+        let currentMonth = calendar.component(.month, from: currentDate)
+        let currentDay = calendar.component(.day, from: currentDate)
+        
+        // 입력된 시간의 년, 월, 일
+        let year = calendar.component(.year, from: date)
+        let month = calendar.component(.month, from: date)
+        let day = calendar.component(.day, from: date)
+        
+        // 입력된 시간의 시, 분
+        let hour = calendar.component(.hour, from: date)
+        let minute = calendar.component(.minute, from: date)
+        
+        // 날짜가 오늘 이상인지 확인
+        guard currentYear == year && currentMonth == month && currentDay == day else {
+            return false
+        }
+        
+        switch mealType {
+        case 0:
+            // 아침: ~ 8:20
+            return hour < 8 || (hour == 8 && minute <= 20)
+        case 1:
+            // 점심: 8:21 ~ 13:30
+            return (hour == 8 && minute > 20) || (hour > 8 && hour < 13) || (hour == 13 && minute <= 30)
+        case 2:
+            // 저녁: 13:31 ~ 19:10
+            return (hour == 13 && minute > 30) || (hour > 13 && hour < 19) || (hour == 19 && minute <= 10)
+        default:
+            return false
         }
     }
 }
