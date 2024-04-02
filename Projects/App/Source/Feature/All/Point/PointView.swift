e4d9240d8d19e5ec7ef6ad0f2aa9ca3b358996e9
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2024-04-03 00:41:57 +0900

    feat: Create point view, point view model

---
+++ Projects/App/Source/Feature/All/Point/PointView.swift
@@ -0,0 +1,8 @@
+//
+//  PointView.swift
+//  DodamDodam
+//
+//  Created by 이민규 on 4/3/24.
+//
+
+import Foundation
---
+++ Projects/App/Source/Feature/All/Point/PointViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PointViewModel.swift
+//  DodamDodam
+//
+//  Created by 이민규 on 4/3/24.
+//
+
+import Foundation
