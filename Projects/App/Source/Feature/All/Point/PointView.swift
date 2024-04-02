1b023050762bb8dc1fd0d27a5c0a383cf659acca
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2024-04-03 00:42:35 +0900

    feat: Create point view, point view model

--- Projects/App/Source/Feature/All/Point/PointView.swift
+++ Projects/App/Source/Feature/All/Point/PointView.swift
@@ -5,4 +5,30 @@
 //  Created by 이민규 on 4/3/24.
 //
 
-import Foundation
+import SwiftUI
+import DDS
+
+struct PointView: View {
+    
+    @InjectObject var viewModel: PointViewModel
+    @Flow var flow
+    
+    var body: some View {
+        DodamScrollView.default(title: "내 상벌점") {
+            VStack(spacing: 24) {
+                
+            }
+            .padding([.top, .horizontal], 16)
+        }
+        .bottomMask()
+        .task {
+            await viewModel.onAppear()
+        }
+    }
+}
+
+#Preview {
+    FlowPreview {
+        PointView()
+    }
+}
--- Projects/App/Source/Feature/All/Point/PointViewModel.swift
+++ Projects/App/Source/Feature/All/Point/PointViewModel.swift
@@ -5,4 +5,53 @@
 //  Created by 이민규 on 4/3/24.
 //
 
-import Foundation
+import Combine
+
+class PointViewModel: ObservableObject {
+    
+    // MARK: - State
+    @Published var domitoryPointData: [PointResponse]?
+    @Published var schoolPointData: [PointResponse]?
+    @Published var domitoryPointScoreData: [PointScoreResponse]?
+    @Published var schoolPointScoreData: [PointScoreResponse]?
+    
+    // MARK: - Repository
+    @Inject var pointRepository: any PointRepository
+    
+    // MARK: - Method
+    @MainActor
+    func onAppear() async {
+        
+        await fetchPointScore()
+    }
+    
+    @MainActor
+    func fetchPoint() async {
+        
+        do {
+            domitoryPointData = try await pointRepository.fetchPoint(
+                .init(type: .dormitory)
+            )
+            schoolPointData = try await pointRepository.fetchPoint(
+                .init(type: .school)
+            )
+        } catch let error {
+            print(error)
+        }
+    }
+    
+    @MainActor
+    func fetchPointScore() async {
+        
+        do {
+            domitoryPointScoreData = try await pointRepository.fetchPointScore(
+                .init(type: .dormitory)
+            )
+            schoolPointScoreData = try await pointRepository.fetchPointScore(
+                .init(type: .school)
+            )
+        } catch let error {
+            print(error)
+        }
+    }
+}
