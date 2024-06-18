a3dff3783519a721492e7d41e0394d41435bc971
leeyoonchae <yoonchae2015@gmail.com>
2024-06-18 20:14:22 +0900

    fix : Fix Interceptor

--- Projects/App/Source/Data/Network/Remote/RemoteInterceptor.swift
+++ Projects/App/Source/Data/Network/Remote/RemoteInterceptor.swift
@@ -9,26 +9,36 @@ import Foundation
 import Alamofire
 import SignKit
 
-struct RemoteInterceptor: RequestInterceptor {
+class RemoteInterceptor: RequestInterceptor {
     
     func retry(
         _ request: Request,
         for session: Session,
         dueTo error: Error,
         completion: @escaping (RetryResult) -> Void
-    ) async {
+    ) {
         guard let response = request.task?.response as? HTTPURLResponse,
               response.statusCode == 401 else {
             completion(.doNotRetryWithError(error))
             return
         }
-        if let refreshToken = Sign.refreshToken {
-            @Inject var authRepository: any AuthRepository
+        
+        guard let refreshToken = Sign.refreshToken else {
+            completion(.doNotRetryWithError(error))
+            return
+        }
+        
+        @Inject var authRepository: any AuthRepository
+        
+        Task {
             do {
                 try await authRepository.postReissue(
                     .init(refreshToken: refreshToken)
                 )
                 completion(.retry)
+//                DispatchQueue.main.async {
+//                    completion(.retry)
+//                }
             } catch {
                 if let id = Sign.id,
                    let pw = Sign.password {
@@ -36,13 +46,21 @@ struct RemoteInterceptor: RequestInterceptor {
                         _ = try await authRepository.postLogin(
                             .init(id: id, pw: pw)
                         )
+                        completion(.retry)
+//                        DispatchQueue.main.async {
+//                            completion(.retry)
+//                        }
                     } catch let error {
+                        DispatchQueue.main.async {
+                            completion(.doNotRetryWithError(error))
+                        }
+                    }
+                } else {
+                    DispatchQueue.main.async {
                         completion(.doNotRetryWithError(error))
                     }
                 }
             }
-        } else {
-            completion(.doNotRetryWithError(error))
         }
     }
 }
