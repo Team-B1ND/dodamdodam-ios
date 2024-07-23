9b35e6d3808493773e0864cbf3d2b571a8f66493
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-23 10:54:51 +0900

    feat: Move features to Feature module

---
+++ Projects/Shared/Source/Flow/Flow.swift
@@ -0,0 +1,29 @@
+import FlowKit
+import SwiftUI
+
+@available(iOS 13, *)
+@propertyWrapper
+public struct Flow: DynamicProperty {
+  
+  @EnvironmentObject private var object: FlowProvider
+  
+  public var wrappedValue: FlowProvider {
+    object
+  }
+  
+  public init() { }
+}
+
+public struct FlowPreview<C: View>: View {
+    
+    private let content: () -> C
+    
+    public init(@ViewBuilder content: @escaping () -> C) {
+        self.content = content
+    }
+    
+    public var body: some View {
+        FlowPresenter(rootView: content())
+            .ignoresSafeArea()
+    }
+}
