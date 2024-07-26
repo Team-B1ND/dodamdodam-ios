2f0ad8eba783d10eee5ce9aa733b741eefd57b9b
hhhello <hhhello0507@dgsw.hs.kr>
2024-07-26 15:25:30 +0900

    feat: Style NighApplyCell

--- Projects/Feature/Source/NightStudy/Component/NightStudyApplyCell.swift
+++ Projects/Feature/Source/NightStudy/Component/NightStudyApplyCell.swift
@@ -24,61 +24,43 @@ struct NightStudyApplyCell: View {
     var body: some View {
         VStack(spacing: 16) {
             HStack(spacing: 12) {
-                ZStack {
-                    Text({ () -> String in
-                        switch nightStudyData.status {
-                        case .allowed: 
-                            return "승인됨"
-                        case .pending: 
-                            return "대기중"
-                        case .rejected:
-                            return "거절됨"
-                        }
-                    }())
-//                    .font(.title(.small))
-//                    .dodamColor(.onPrimary)
-                    .padding(.vertical, 4)
-                    .padding(.horizontal, 12)
-                }
-                .frame(height: 27)
-//                .background({ () -> Color in
-//                    switch nightStudyData.status {
-//                    case .allowed: 
-//                        return Dodam.color(.primary)
-//                    case .pending: 
-//                        return Dodam.color(.onSurfaceVariant)
-//                    case .rejected:
-//                        return Dodam.color(.error)
-//                    }
-//                }())
-                .clipShape(RoundedRectangle(cornerRadius: 32))
+                DodamTag({
+                    switch nightStudyData.status {
+                    case .allowed: "승인됨"
+                    case .pending: "대기중"
+                    case .rejected: "거절됨"
+                    }
+                }(), type: {
+                    switch nightStudyData.status {
+                    case .allowed: .primary
+                    case .pending: .secondary
+                    case .rejected: .negative
+                    }
+                }())
                 Spacer()
                 Text({
                     let date = nightStudyData.createdAt.parseString(format: "M월 d일 (E)")
                     return date
                 }())
-//                .font(.label(.large))
-//                .dodamColor(.onSurfaceVariant)
+                .label(.medium)
+                .foreground(DodamColor.Label.normal)
                 .padding(.top, 5)
             }
             .padding([.top, .horizontal], 16)
             VStack(spacing: 12) {
                 Text("\(nightStudyData.content)")
-//                    .font(.body(.medium))
-//                    .dodamColor(.onSurface)
+                    .body1(.medium)
+                    .foreground(DodamColor.Label.normal)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                Rectangle()
-                    .frame(height: 1)
-                    .frame(maxWidth: .infinity)
-//                    .foregroundStyle(Dodam.color(.secondary))
+                DodamDivider()
                 if nightStudyData.status == .rejected {
                     HStack(spacing: 8) {
                         Text("거절 사유")
-//                            .font(.label(.large))
-//                            .dodamColor(.onSurfaceVariant)
+                            .label(.regular)
+                            .foreground(DodamColor.Label.alternative)
                         Text("\(nightStudyData.rejectReason ?? "선생님께서 심야 자습 신청을 거절하였습니다")")
                             .font(.system(size: 16, weight: .medium))
-//                            .dodamColor(.onSurface)
+                            .foreground(DodamColor.Label.normal)
                         Spacer()
                     }
                     .padding(.top, 4)
@@ -87,18 +69,18 @@ struct NightStudyApplyCell: View {
                         VStack(spacing: 8) {
                             HStack(alignment: .bottom, spacing: 4) {
                                 Text(nightStudyData.startAt.parseString(format: "M월 d일"))
-//                                    .font(.body(.medium))
-//                                    .dodamColor(.onSurface)
+                                    .body1(.medium)
+                                    .foreground(DodamColor.Label.normal)
                                 Text("시작")
-//                                    .font(.label(.large))
-//                                    .dodamColor(.onSurfaceVariant)
+                                    .label(.medium)
+                                    .foreground(DodamColor.Label.normal)
                                 Spacer()
                                 Text(nightStudyData.endAt.parseString(format: "M월 d일"))
-//                                    .font(.body(.medium))
-//                                    .dodamColor(.onSurface)
+                                    .body1(.medium)
+                                    .foreground(DodamColor.Label.normal)
                                 Text("종료")
-//                                    .font(.label(.large))
-//                                    .dodamColor(.onSurfaceVariant)
+                                    .label(.medium)
+                                    .foreground(DodamColor.Label.normal)
                             }
                             DodamLinearProgressView(
                                 progress: calculatingDateProgress(
@@ -114,11 +96,11 @@ struct NightStudyApplyCell: View {
                        nightStudyData.doNeedPhone {
                         HStack(spacing: 8) {
                             Text("휴대폰 사유")
-//                                .font(.label(.large))
-//                                .dodamColor(.onSurfaceVariant)
+                                .label(.regular)
+                                .foreground(DodamColor.Label.alternative)
                             Text("\(reasonForPhone)")
-                                .font(.system(size: 16, weight: .medium))
-//                                .dodamColor(.onSurface)
+                                .label(.medium)
+                                .foreground(DodamColor.Label.normal)
                             Spacer()
                         }
                         .padding(.top, 4)
@@ -127,7 +109,7 @@ struct NightStudyApplyCell: View {
             }
             .padding([.bottom, .horizontal], 16)
         }
-//        .background(Dodam.color(.surfaceContainer))
-        .clipShape(RoundedRectangle(cornerRadius: 18))
+        .background(DodamColor.Background.normal)
+        .clipShape(.large)
     }
 }
