94fb84c1987b7e6641abe0be9566b70ef6cc328e
JoYoungwoo <roy03280328@gmail.com>
2024-11-18 16:12:55 +0900

    feat: Create ScheduleContainer

--- Projects/Feature/Source/Home/Component/ScheduleContainer.swift
+++ Projects/Feature/Source/Home/Component/ScheduleContainer.swift
@@ -13,6 +13,26 @@ struct ScheduleContainer: View {
     
     private let scheduleData: [ScheduleResponse]?
     
+    func getSchedule(scheduleResponse: [ScheduleResponse]) -> [ScheduleResponse] {
+        var schedules: [ScheduleResponse] = []
+        var cnt: Int = 0
+        
+        for data in scheduleResponse {
+            if schedules.count == 0 {
+                schedules.append(scheduleResponse[0])
+            } else if schedules[0].date == data.date || cnt <= 1 {
+                schedules.append(data)
+                if schedules[0].date != data.date {
+                    cnt += 1
+                }
+            }
+        }
+    
+    
+        print("스케줄 \(schedules)")
+        return schedules
+    }
+    
     init(
         data scheduleData: [ScheduleResponse]?
     ) {
@@ -20,53 +40,52 @@ struct ScheduleContainer: View {
     }
     
     var body: some View {
-        if let data = scheduleData {
-            HStack(alignment: .top, spacing: 12) {
-                ForEach(data, id: \.self) { data in
-                    VStack(alignment: .leading, spacing: 0) {
-                        Text("\(data.name)")
-//                        Text("D - " + { () -> String in
-//                            let dateFormatter = DateFormatter()
-//                            dateFormatter.dateFormat = "yyyy-MM-dd"
-//                            let currentDate = Date()
-//                            guard let targetDate = dateFormatter.date(from: data.) else {
-//                                return "오류"
-//                            }
-//                            let calendar = Calendar.current
-//                            let dateComponents = calendar.dateComponents([.day], from: currentDate, to: targetDate)
-//                            
-//                            guard let daysDifference = dateComponents.day else {
-//                                return "오류"
-//                            }
-//                            if daysDifference == 0 {
-//                                return "day"
-//                            } else {
-//                                return "\(abs(daysDifference))"
-//                            }
-//                        }())
-//                        .font(.title(.small))
-//                        .dodamColor(.onSurfaceVariant)
-//                        
-//                        Text({ () -> String in
-//                            let dateFormatter = DateFormatter()
-//                            dateFormatter.dateFormat = "yyyy-MM-dd"
-//                            dateFormatter.locale = Locale(identifier: "ko_KR")
-//                            if let date = dateFormatter.date(from: data.startDate) {
-//                                dateFormatter.dateFormat = "M월 d일 (E)"
-//                                return dateFormatter.string(from: date)
-//                            }
-//                            return "오류"
-//                        }())
-//                        .font(.label(.medium))
-//                        .dodamColor(.tertiary)
+        VStack(spacing: 10) {
+            if let data = scheduleData {
+                ForEach(0 ..< min(data.count, 2), id: \.self) { idx in
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack(alignment: .bottom, spacing: 4) {
+                            Text("\(data[idx].date[0].suffix(2))일")
+                                .heading2(.bold)
+                                .foreground(DodamColor.Label.normal)
+                            
+                            let weekdayName = { (dateString: String) -> String in
+                                let dateFormatter = DateFormatter()
+                                dateFormatter.dateFormat = "yyyy-MM-dd"
+                                
+                                guard let date = dateFormatter.date(from: dateString) else {
+                                    return "알 수 없음"
+                                }
+                                
+                                let calendar = Calendar.current
+                                let weekdayIndex = calendar.component(.weekday, from: date)
+                                return calendar.weekdaySymbols[weekdayIndex - 1]
+                            }
+                            
+                            Text("\(weekdayName(data[idx].date[0]))")
+                                .label(.medium)
+                                .foreground(DodamColor.Label.alternative)
+                            
+                        }
+                        
+                        HStack {
+                            Circle()
+                                .frame(width: 6, height: 6)
+                                .foregroundStyle(Color.init(0xffFFB5B5))
+                            Text("\(data[idx].name)")
+                            Spacer()
+                        }
                     }
+                    .padding(.bottom, 10)
+                    .padding(.horizontal, 10)
                 }
+            } else {
+                SupportingContainer(
+                    subTitle: "한달 간 일정이 없어요",
+                    title: "다음 달 일정을 기다려주세요"
+                )
             }
-        } else {
-            SupportingContainer(
-                subTitle: "한달 간 일정이 없어요",
-                title: "전체 일정 확인하기"
-            )
         }
+        .padding(.top, 6)
     }
 }
