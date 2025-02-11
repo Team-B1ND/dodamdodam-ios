ce089da34bbcd5f591b75afab4e87d9e5464e833
leeyoonchae <yoonchae2015@gmail.com>
2025-02-11 18:08:39 +0900

    feat : DivisionWaiting

--- Projects/Feature/Source/Division/Waiting/DivisionWaitingMemberView.swift
+++ Projects/Feature/Source/Division/Waiting/DivisionWaitingMemberView.swift
@@ -11,39 +11,73 @@ import SwiftUI
 struct DivisionWaitingMemberView: View {
     @State private var selectedMember: Member?
     @State private var isSheetPresented = false
+    @StateObject private var viewModel = DivisionWaitingViewModel()
+    let divisionId: Int
     
     var body: some View {
         DodamScrollView.medium(title: "‘B1ND’ 그룹\n가입 신청 대기 중인 멤버") {
             VStack(alignment: .leading, spacing: 0) {
-                MemberSection(title: "학생", members: [
-                    Member(name: "테스트1", imageUrl: nil)
-                ], onSelect: showMemberSheet, showDivider: false)
-                
-                MemberSection(title: "학부모", members: [
-                    Member(name: "테스트2", imageUrl: nil)
-                ], onSelect: showMemberSheet, showDivider: false)
+                if viewModel.divisionMember?.isEmpty ?? true {
+                    Text("대기 중인 멤버가 없습니다.")
+                        .body1(.medium)
+                        .foreground(DodamColor.Label.assistive)
+                        .padding()
+                } else {
+                    MemberSection(
+                        title: "가입 신청 멤버",
+                        members: members,
+                        onSelect: showMemberSheet,
+                        showDivider: false
+                    )
+                }
             }
             .padding(16)
+        }
+        .refreshable {
+            await viewModel.fetchAllData(id: divisionId)
+        }
+        .task {
+            await viewModel.fetchAllData(id: divisionId)
+        }
+        .onChange(of: selectedMember) { newValue in
+            isSheetPresented = newValue != nil
         }
         .sheet(isPresented: $isSheetPresented) {
             if let member = selectedMember {
                 if #available(iOS 16.0, *) {
-                    WaitingMemberSheetView(member: member)
+                    WaitingMemberSheetView(member: member, id: divisionId)
                         .presentationDragIndicator(.visible)
                         .presentationDetents([.height(dynamicHeight(for: member))])
                 } else {
-                    //
+                    WaitingMemberSheetView(member: member, id: divisionId)
                 }
+            } else {
+                Text("멤버 정보를 불러올 수 없습니다.")
             }
         }
+
         .background(DodamColor.Background.normal)
     }
     
+    private var members: [Member] {
+        viewModel.divisionMember?.map {
+            Member(
+                id: $0.id,
+                name: $0.memberName,
+                role: $0.permission.rawValue,
+                imageUrl: $0.profileImage,
+                grade: $0.grade,
+                room: $0.room,
+                number: $0.number
+            )
+        } ?? []
+    }
+
     private func showMemberSheet(member: Member) {
         selectedMember = member
         isSheetPresented = true
     }
-    
+
     private func dynamicHeight(for member: Member) -> CGFloat {
         let baseHeight: CGFloat = 210
         let additionalHeight: CGFloat = CGFloat(member.role?.count ?? 0) * 5
--- Projects/Feature/Source/Division/Waiting/WaitingMemberSheetView.swift
+++ Projects/Feature/Source/Division/Waiting/WaitingMemberSheetView.swift
@@ -10,26 +10,34 @@ import SwiftUI
 
 struct WaitingMemberSheetView: View {
     let member: Member
-    
+    let id: Int
+    @StateObject private var viewModel = DivisionWaitingViewModel()
+
     var body: some View {
         VStack(alignment: .leading) {
             Text("\(member.name)님 정보")
                 .heading1(.bold)
                 .foreground(DodamColor.Label.normal)
-            
-            Text("직군 | 학생")
+
+            Text("직군 | \(member.role ?? "대기 중")")
                 .headline(.medium)
                 .foreground(DodamColor.Label.assistive)
                 .padding(.top, 8)
-            
-            Text("학번 | 1학년 2반 13번")
+
+            Text("학번 | \(member.grade)학년 \(member.room)반 \(member.number)번")
                 .headline(.medium)
                 .foreground(DodamColor.Label.assistive)
                 .padding(.top, 8)
-            
+
             HStack(alignment: .center) {
                 Button {
-                    //
+                    guard let memberId = member.id else {
+                        print("id 값이 없음")
+                        return
+                    }
+                    Task {
+                        await viewModel.deleteMembers(id: id, idList: [memberId])
+                    }
                 } label: {
                     Text("거절하기")
                         .body2(.bold)
@@ -39,8 +47,11 @@ struct WaitingMemberSheetView: View {
                         .cornerRadius(12)
                 }
                 .padding(.trailing, 8)
+
                 Button {
-                    //
+                    Task {
+                        await viewModel.addMembers(id: id, memberId: member.name)
+                    }
                 } label: {
                     Text("승인하기")
                         .body2(.bold)
