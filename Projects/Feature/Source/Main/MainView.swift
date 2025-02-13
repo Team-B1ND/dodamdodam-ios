cc2007d16058c683c1f5bce14f80a3e0a7b3b5b4
hhhello <hhhello0507@dgsw.hs.kr>
2025-02-13 19:09:27 +0900

    fix: SupportingContainer padding

--- Projects/Feature/Source/Main/MainView.swift
+++ Projects/Feature/Source/Main/MainView.swift
@@ -16,7 +16,6 @@ public struct MainView: View {
     public init() {}
     
     public var body: some View {
-        return RegisterSelectRoleView()
         DodamTabView(selection: $selection) {
             HomeView(selection: $selection)
                 .page(.icon(.home))
