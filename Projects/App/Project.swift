3af70fe04c613aac2410fae2a98ab55adf111336
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2025-06-07 13:28:42 +0900

    build: Update version to 3.4.3

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -26,7 +26,7 @@ let project = Project(
             infoPlist: .extendingDefault(
                 with: [
                     "CFBundleDisplayName": "도담도담",
-                    "CFBundleShortVersionString": "3.4.2",
+                    "CFBundleShortVersionString": "3.4.3",
                     "CFBundleVersion": "1",
                     "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"],
                     "UIMainStoryboardFile": "",
