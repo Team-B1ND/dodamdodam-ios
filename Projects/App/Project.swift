763eef1d0ba2b703e31464a5f457e13c2c4f16a1
김은찬 <164847037+eunchan2815@users.noreply.github.com>
2025-07-11 16:28:09 +0900

    chore: Update version to 3.4.4

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -26,7 +26,7 @@ let project = Project(
             infoPlist: .extendingDefault(
                 with: [
                     "CFBundleDisplayName": "도담도담",
-                    "CFBundleShortVersionString": "3.4.3",
+                    "CFBundleShortVersionString": "3.4.4",
                     "CFBundleVersion": "1",
                     "UISupportedInterfaceOrientations": ["UIInterfaceOrientationPortrait"],
                     "UIMainStoryboardFile": "",
